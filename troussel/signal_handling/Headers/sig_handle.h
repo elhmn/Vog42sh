01e35db850426a58df679e6607362ced3541e64b
Tony ROUSSEL <troussel@e3r13p12.42.fr>
2014-02-24 14:55:03 +0100

    sig_h update

--- troussel/signal_handling/Headers/sig_handle.h
+++ troussel/signal_handling/Headers/sig_handle.h
@@ -6,12 +6,14 @@
 /*   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/24 14:20:08 by troussel          #+#    #+#             */
-/*   Updated: 2014/02/24 14:29:00 by troussel         ###   ########.fr       */
+/*   Updated: 2014/02/24 14:39:37 by troussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef		SIGHANDLE_H
 # define	SIGHANDLE_H
 # include	<signal.h>
 # include	"ftprintf.h"
+
+void	sig_handle(void);
 
 #endif		/* !SIGHANDLE_H */
--- troussel/signal_handling/srcs/signal_handler.c
+++ troussel/signal_handling/srcs/signal_handler.c
@@ -6,6 +6,21 @@
 /*   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/24 14:23:31 by troussel          #+#    #+#             */
-/*   Updated: 2014/02/24 14:26:13 by troussel         ###   ########.fr       */
+/*   Updated: 2014/02/24 14:54:27 by troussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "sig_handle.h"
+
+static
+
+void		sig_handle(void)
+{
+	signal(SIGINT, sig_resp);
+	signal(SIGINFO, sig_resp);
+	signal(SIGUSR1, sig_resp);
+	signal(SIGUSR2, sig_resp);
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGSEGV, sig_resp);
+	signal(SIGBUS, sig_resp);
+	signal(SIGTSTP, SIG_IGN);
+}
