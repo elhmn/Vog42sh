0139e47d7c5c5f56d69d650ecb8a86da8ad8001b
Tony ROUSSEL <troussel@e3r4p6.42.fr>
2014-02-25 15:25:22 +0100

    main updt

--- troussel/main/srcs/main.c
+++ troussel/main/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/25 13:37:29 by troussel          #+#    #+#             */
-/*   Updated: 2014/02/25 15:11:54 by troussel         ###   ########.fr       */
+/*   Updated: 2014/02/25 15:13:28 by troussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "main_sh.h"
@@ -17,6 +17,5 @@ int		main(void)
 {
 	if (!environ[0])
 		puterror(0, "Environment data is empty", 1);
-
 	return (0);
 }
