3d5a72ce513e917c52a08928a00ee3ed8ac48380
Tony ROUSSEL <troussel@e3r4p6.42.fr>
2014-02-25 15:04:56 +0100

    error function done

--- troussel/error_manage/srcs/error.c
+++ troussel/error_manage/srcs/error.c
@@ -6,22 +6,41 @@
 /*   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/25 13:52:07 by troussel          #+#    #+#             */
-/*   Updated: 2014/02/25 14:41:21 by troussel         ###   ########.fr       */
+/*   Updated: 2014/02/25 15:04:35 by troussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "error_sh.h"
 
 static void	code_seq(int code)
 {
-
+	if (code == CMDNF)
+		puterror(": command not found\n");
+	else if (code == ARG_M)
+		puterror("Too many arguments\n");
+	else if (code == ARG_F)
+		puterror("Too few arguments\n");
+	else if (code == PERM)
+		puterror("Permission denied\n");
+	else if (code >= UNDEF)
+		puterror("Undefined error\n");
 }
 
 void		error(int code, char *str, int esc_seq)
 {
 	if (str)
+	{
+		puterror("42sh: ");
 		puterror(str);
+	}
+	if (code && str)
+		puterror(": ");
 	if (code)
 		code_seq(code);
 	if (esc_seq)
-		exit(1);
+	{
+		puterror("exit\n");
+		if (code)
+			exit(code);
+		exit(-1);
+	}
 }
