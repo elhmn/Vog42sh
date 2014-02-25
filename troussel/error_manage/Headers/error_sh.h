aa1c4bec2a366654f9450a5ca3409ba9d7339795
Tony ROUSSEL <troussel@e3r4p6.42.fr>
2014-02-25 14:45:56 +0100

    error updt

--- troussel/error_manage/Headers/error_sh.h
+++ troussel/error_manage/Headers/error_sh.h
@@ -6,12 +6,18 @@
 /*   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/25 13:46:37 by troussel          #+#    #+#             */
-/*   Updated: 2014/02/25 13:48:08 by troussel         ###   ########.fr       */
+/*   Updated: 2014/02/25 14:45:21 by troussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef		ERRORSH_H
 # define	ERRORSH_H
+# include	<stdlib.h>
+
+# define	CMDNF 1
+# define	ARG_M 2
+# define	ARG_F 3
 
 void	puterror(char const *s);
+void	error(int code, char *str, int esc_seq);
 
 #endif		/* !ERRORSH_H */
--- troussel/error_manage/srcs/error.c
+++ troussel/error_manage/srcs/error.c
@@ -6,12 +6,22 @@
 /*   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/25 13:52:07 by troussel          #+#    #+#             */
-/*   Updated: 2014/02/25 13:56:30 by troussel         ###   ########.fr       */
+/*   Updated: 2014/02/25 14:41:21 by troussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "error_sh.h"
 
-void	error(int code, char *str, int esc_seq)
+static void	code_seq(int code)
 {
 
+}
+
+void		error(int code, char *str, int esc_seq)
+{
+	if (str)
+		puterror(str);
+	if (code)
+		code_seq(code);
+	if (esc_seq)
+		exit(1);
 }
