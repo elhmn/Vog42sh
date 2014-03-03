dd48ba42609a18d8b522c0ff59d09beab6db688e
Julien Blancher <jblanche@student.42.fr>
2014-03-03 13:40:15 +0100

    aujd

--- jblanche/lexer.c
+++ jblanche/lexer.c
@@ -6,15 +6,20 @@
 /*   By: jblanche <jblanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/28 17:48:45 by jblanche          #+#    #+#             */
-/*   Updated: 2014/02/28 18:59:57 by jblanche         ###   ########.fr       */
+/*   Updated: 2014/03/03 13:36:57 by jblanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "42sh.h"
+
+// Reception: cd dev && cp ../img/toto.jpg titi && env -i &&
+
+
 
 t_lex			*lexer(char *str)
 {
 	t_lex		*lex;
 
 	lex = init_lex(lex);
+	
 }
