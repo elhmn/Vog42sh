92698719d2010aaf1a0a71dfb26064ff7ab9543b
Tony ROUSSEL <troussel@e3r12p21.42.fr>
2014-03-07 11:22:43 +0100

    ajout debut node_run, ftshenv.h, maj parse_tree

---
+++ troussel/node_run/srcs/node_run.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   node_run.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/28 16:56:28 by troussel          #+#    #+#             */
+/*   Updated: 2014/02/28 17:52:32 by troussel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "42sh.h"
+#include "node_run.h"
+#include <unistd.h>
+
+static int		launch(t_cmd *dat, t_env *env, int pip[2][2], int swtch)
+{
+		fk = fork();
+		if (!fk)
+			execute(dat, env, pip, swtch);
+		else
+			waitpid(fk, &env->last_ret, WUNTRACED);
+		return (env->last_return);
+}
+
+int				node_run(t_cmd *dat, t_env *env)
+{
+	static int	swtch = 1;
+	static int	pip[2][2];
+	pid_t		fk;
+	int			retval;
+
+	swtch = (swtch ? 0 : 1);
+	if (!pipe_w && !pipe_r && isbuiltin(dat))
+	{
+		env->last_ret = execute(dat, env, pip, swtch);
+		return (env->last_ret);
+	}
+	if (dat->pipe_w)
+	{
+		if (setpipe(dat, &pip, swtch))
+			return ((env->last_ret = 1));
+	}
+	if (dat->ifile || dat->ofile)
+	{
+		if (setfdfil(dat, &pip, swtch))
+			return ((env->last_ret = 1))
+	}
+	if (isbuiltin(dat) || seekbin(dat, env->path))
+		return (launch(dat, env, pip, swtch);
+	else
+		return ((env->last_ret = 127));
+}
---
+++ troussel/node_run/srcs/setfd.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   setfd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/02/28 16:56:28 by troussel          #+#    #+#             */
+/*   Updated: 2014/02/28 17:52:32 by troussel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "42sh.h"
+#include "node_run.h"
+#include "error_sh.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+static int		openfile(char *file, int in, int appmode)
+{
+	if (appmode)
+		return (open(file, O_CREAT | O_RDWR | O_APPEND, S_IROTH | S_IWUSR
+					 | S_IWGRP | S_IRUSR | S_IRGRP));
+	else if (!in)
+		return (open(file, O_CREAT | O_RDWR, S_IROTH | S_IWUSR | S_IWGRP
+					 | S_IRUSR | S_IRGRP));
+	else if (in)
+		return (open(file, O_RDONLY));
+}
+
+int				setpipe(t_cmd *dat, int pip[1][2][2], int swtch)
+{
+	if (pipe(pip[0][swtch]) == -1)
+	{
+		error(0, "pipe error", 0);
+		return (1);
+	}
+	else
+		return (0);
+}
+
+int				setfdfil(t_cmd *dat, int pip[1][2][2], int swtch)
+{
+	int	error;
+
+	error = 0;
+	if (dat->ifile)
+	{
+		if (dat->pipe_r)
+			close(pip[0][!swtch][0]);
+		if ((pip[0][!swtch][0] = openf*ile(dat->ifile, 1, 0)) == -1)
+		{
+			error(UNDEF, dat->ifile, 0);
+			++error;
+		}
+	}
+	if (dat->ofile)
+	{
+		if (dat->pipe_w)
+			close(pip[0][swtch][1]);
+		if ((pip[0][swtch][1] = openfile(dat->ofile, 0, dat->fld_app)) == -1)
+		{
+			error(UNDEF, dat->ofile, 0);
+			++error;
+		}
+	}
+	return (error);
+}
