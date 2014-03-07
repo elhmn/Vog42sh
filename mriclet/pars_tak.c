4402ea3ea2787b4c87ce04a3469e88ad498b8027
Mathieu RICLET <mriclet@e3r11p22.42.fr>
2014-03-07 16:31:37 +0100

    pars_tak

---
+++ mriclet/pars_tak.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pars_tak.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mriclet <mriclet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/03/07 14:53:53 by mriclet           #+#    #+#             */
+/*   Updated: 2014/03/07 16:23:48 by mriclet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+t_for		*alloc_new_tree(t_takl *lst, int opt, t_for *prev)
+{
+	t_for	*ret;
+
+	ret = ft_memalloc(sizeof(t_for *));
+	ret->wood = ft_memalloc(sizeof(t_tree *));
+	if (opt == 0)
+		ret->wood = parse_tree(lst->lst, NULL, 0);
+	else
+		ret->nxt->wood = parse_tree(lst->lst, prev->wood->data , prev_pipe_w);
+	return (ret);
+}
+
+t_for		*pars_for(t_tokl *lst)
+{
+	t_for	*ret;
+	t_for	*elem;
+	int		loop;
+	int		prev_pipe_w;
+
+	loop = 1;
+	elem = alloc_new_tree(lst, 0, NULL);
+	first = elem;
+	prev_pipe_w = elem->wood->data->pipe_w;
+	while (loop)
+	{
+		elem->nxt = alloc_new_tree(lst, 1, elem);
+		prev_pipe_w = elem->wood->data->pipe_w;
+		if (!elem->wood->nxt || !lst->nxt)
+			loop = 0;
+		else
+		{
+			elem->wood = elem->wood->nxt;
+			lst = lst->nxt;
+			elem = elem->nxt;
+		}
+	}
+	elem->nxt = NULL;
+	return (first);
+}
+
+void		*ft_memalloc(size_t size)
+{
+	void	ret;
+
+	ret = malloc(size);
+	if (!ret)
+		exit(1);
+	return (ret);
+}
