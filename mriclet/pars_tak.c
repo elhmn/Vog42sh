cda4581c296bd52737326e59a575638c0297e226
Mathieu RICLET <mriclet@e3r11p22.42.fr>
2014-03-07 16:57:08 +0100

    pars_tak

--- mriclet/pars_tak.c
+++ mriclet/pars_tak.c
@@ -6,11 +6,11 @@
 /*   By: mriclet <mriclet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/07 14:53:53 by mriclet           #+#    #+#             */
-/*   Updated: 2014/03/07 16:23:48 by mriclet          ###   ########.fr       */
+/*   Updated: 2014/03/07 16:54:12 by mriclet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-t_for		*alloc_new_tree(t_takl *lst, int opt, t_for *prev)
+t_for		*alloc_new_tree(t_takl *lst, int opt, t_for *prev, int prev_pipe_w)
 {
 	t_for	*ret;
 
@@ -31,13 +31,13 @@ t_for		*pars_for(t_tokl *lst)
 	int		prev_pipe_w;
 
 	loop = 1;
-	elem = alloc_new_tree(lst, 0, NULL);
+	elem = alloc_new_tree(lst, 0, NULL, 0);
 	first = elem;
 	prev_pipe_w = elem->wood->data->pipe_w;
 	while (loop)
 	{
-		elem->nxt = alloc_new_tree(lst, 1, elem);
 		prev_pipe_w = elem->wood->data->pipe_w;
+		elem->nxt = alloc_new_tree(lst, 1, elem, prev_pipe_w);
 		if (!elem->wood->nxt || !lst->nxt)
 			loop = 0;
 		else
