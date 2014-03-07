9661b2529fb0339d4f32d6f600f01d7fc48a1f8d
Tony ROUSSEL <troussel@e3r12p21.42.fr>
2014-03-07 15:21:13 +0100

    correction proto execute

--- troussel/node_run/Headers/node_run.h
+++ troussel/node_run/Headers/node_run.h
@@ -6,13 +6,13 @@
 /*   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/28 17:06:14 by troussel          #+#    #+#             */
-/*   Updated: 2014/03/03 16:36:04 by troussel         ###   ########.fr       */
+/*   Updated: 2014/03/07 15:20:09 by troussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef		NODERUN_H
 # define 	NODERUN_H
 
-void	execute(t_cmd *dat, t_env *env, int pip[2][2], int swtch);/*configure les fd et execute le binaire désigné par dat ou, le builtin, dans ce cas la fonction appel exit avec la valeur de retour du builtin*/
+int	execute(t_cmd *dat, t_env *env, int pip[2][2], int swtch);/*configure les fd et execute le binaire désigné par dat ou, le builtin, dans ce cas la fonction appel exit avec la valeur de retour du builtin*/
 
 int	setpipe(t_cmd *dat, int pip[1][2][2], int swtch);
 int	setfdfil(t_cmd *dat, int pip[1][2][2], int swtch);
--- troussel/node_run/srcs/execute.c
+++ troussel/node_run/srcs/execute.c
@@ -6,14 +6,14 @@
 /*   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/07 13:27:51 by troussel          #+#    #+#             */
-/*   Updated: 2014/03/07 13:31:24 by troussel         ###   ########.fr       */
+/*   Updated: 2014/03/07 15:05:19 by troussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ftsh.h"
 #include "node_run.h"
 #include "ftsh_env.h"
 
-void	execute(t_cmd *dat, t_env *env, int pip[2][2], int swtch)
+int		execute(t_cmd *dat, t_env *env, int pip[2][2], int swtch)
 {
 
 }
