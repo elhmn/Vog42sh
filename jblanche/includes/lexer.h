da5e8ea1c756dd88e507646c1fad6a6dda731425
Julien Blancher <jblanche@student.42.fr>
2014-03-18 14:01:32 +0100

    norme

--- jblanche/includes/lexer.h
+++ jblanche/includes/lexer.h
@@ -6,7 +6,7 @@
 /*   By: jblanche <jblanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/28 17:54:08 by jblanche          #+#    #+#             */
-/*   Updated: 2014/03/18 13:21:49 by jblanche         ###   ########.fr       */
+/*   Updated: 2014/03/18 14:00:37 by jblanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 # include "../libft/includes/libft.h"
 # include "lexer.h"
 # include "ftsh.h"
-void		print_lex(t_lex *lex);
-t_lex		*init_lex(t_lex *lex);
-t_lex		*free_lex(t_lex *lex);
-t_lex		*add_lex(t_lex *lex, char *elm, int tok);
-int			is_spec(char *str);
-char		*ft_strtrim_lex(char *s);
+void	print_lex(t_lex *lex);
+t_lex	*init_lex(t_lex *lex);
+t_lex	*free_lex(t_lex *lex);
+t_lex	*add_lex(t_lex *lex, char *elm, int tok);
+int		is_spec(char *str);
+char	*ft_strtrim_lex(char *s);
 int		ft_printf(const char *str, ...)__attribute__((format(printf, 1, 2)));
 #endif /* !LEXER_H */
