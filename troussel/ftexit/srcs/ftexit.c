70cc185f488979836039d9e6dfbfb2982a5ab0ea
Tony ROUSSEL <troussel@e3r11p22.42.fr>
2014-03-25 17:03:09 +0100

    add exit

--- troussel/ftexit/srcs/ftexit.c
+++ troussel/ftexit/srcs/ftexit.c
@@ -6,7 +6,7 @@
 /*   By: troussel <troussel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/25 15:44:42 by troussel          #+#    #+#             */
-/*   Updated: 2014/03/25 16:08:37 by troussel         ###   ########.fr       */
+/*   Updated: 2014/03/25 17:02:26 by troussel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,28 @@
 #include "libft.h"
 #include <unistd.h>
 
+static int	unconvertible(char *arg)
+{
+	if (!ft_isdigit(arg[0]) && (arg[0] != '-' || arg[0] != '+'))
+		return (1);
+	if ((arg[0] == '-' || arg[0] == '+') && !ft_isdigit(arg[1]))
+		return (1);
+	return (0);
+}
+
 int			ft_exit(t_cmd *dat, t_env *env)
 {
-	if (!dat->arg[1] || unconvertible(dat->arg[1]))/*******/
-exit
+	int	arg;
+
+	if (!dat->arg[1] || unconvertible(dat->arg[1]))
+		exit(env->last_ret);
+	arg = ft_atoi(dat->arg[1]);
+	if (arg > 255)
+		exit(env->last_ret);
+	if (arg < 0)
+	{
+		while (arg < 0)
+			arg = 256 + arg;
+	}
+	exit(arg);
 }
