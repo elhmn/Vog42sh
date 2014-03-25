8e8d9f803c87de1fb596ecc930067ace89045655
Julien Blancher <jblanche@student.42.fr>
2014-03-25 15:17:03 +0100

    setenv

--- jblanche/env/ft_setenv.c
+++ jblanche/env/ft_setenv.c
@@ -6,7 +6,7 @@
 /*   By: jblanche <jblanche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/18 12:41:29 by jblanche          #+#    #+#             */
-/*   Updated: 2014/03/25 15:01:49 by troussel         ###   ########.fr       */
+/*   Updated: 2014/03/25 15:16:51 by jblanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,19 @@ int				ft_setenv(t_cmd *dat, t_env *env)
 
 	tmp = NULL;
 	arg = NULL;
-	if (!dat || !env || !env->path || !env->var || !env->env || !dat->arg[1]
-		|| !dat->arg[2])
+	if (!dat || !env || !env->path || !env->var || !env->env || !dat->arg[1])
 		return (-1);
 	tmp = env->var;
 	if ((tmp = find_env_var(env->var, dat->arg[1])))
 	{
 		free(tmp->val);
-		tmp->val = ft_strdup(dat->arg[2]);
+		tmp->val = (dat->arg[2] ? ft_strdup(dat->arg[2]) : NULL);
 	}
 	else
 	{
-		arg = (char *)malloc(sizeof(arg) * ft_strlen(dat->arg[1]));
 		arg = ft_strdup(dat->arg[1]);
 		arg = ft_strjoin(arg, "=");
-		arg = ft_strjoin(arg, dat->arg[2]);
+		arg = (dat->arg[2] ? ft_strjoin(arg, dat->arg[2]) : arg);
 		env->var = add_venv(arg, env->var);
 	}
 	free(arg);
