dce53ba69b7eee59ac17335c91ae33cced705bed
Boris MBARGA <bmbarga@e3r10p20.42.fr>
2014-03-26 17:38:31 +0100

    fg

---
+++ srcs/free_env.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_env.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmbarga <bmbarga@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/03/26 17:15:29 by bmbarga           #+#    #+#             */
+/*   Updated: 2014/03/26 17:38:17 by bmbarga          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ftsh_env.h"
+#include "ftsh.h"
+#include <stdlib.h>
+
+
+static void free_path(t_path *path)
+{
+	if (path)
+	{
+		free(path->dir);
+		if (path->next)
+			free_var(varenv->next);
+		free(path);
+	}
+}
+
+static void free_var(t_venv *varenv)
+{
+	if (varenv)
+	{
+		free(varenv->val);
+		free(varenv->var);
+		if (varenv->next)
+			free_var(varenv->next);
+		free(varenv);
+	}
+}
+
+void	free_env(t_env *env)
+{
+	free_path(env->path);
+	free_path(env->var);
+	free(env);
+}
